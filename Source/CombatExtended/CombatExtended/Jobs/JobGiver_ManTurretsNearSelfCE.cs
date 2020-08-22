89e60fd0315ad6a77bb803f2cba3f2631f818cc7
simplyWiri <48577820+simplyWiri@users.noreply.github.com>
2020-08-22 20:12:35 +1000

    Updating Man Turrets
    - Same type of deal, getting ammo is still pretty slow, don't have a good answer for it.

--- Source/CombatExtended/CombatExtended/Jobs/JobGiver_ManTurretsNearSelfCE.cs
+++ Source/CombatExtended/CombatExtended/Jobs/JobGiver_ManTurretsNearSelfCE.cs
@@ -1,4 +1,5 @@
 ﻿using RimWorld;
+using System.Linq;
 using Verse;
 using Verse.AI;
 
@@ -13,15 +14,18 @@ namespace CombatExtended
         /// <remarks>Overriden to avoid invalid type cast exception.</remarks>
         public override Job TryGiveJob(Pawn pawn)
         {
+            var turrets = pawn.Map.GetComponent<MapComponent_TurretTracker>().Turrets.Where(t => 
+                t.def.hasInteractionCell && 
+                t.def.HasComp(typeof(CompMannable)) && 
+                pawn.CanReserve(t) && 
+                FindAmmoForTurret(pawn, t) != null);
+
             var thing = GenClosest.ClosestThingReachable(
                 GetRoot(pawn),
                 pawn.Map,
-                ThingRequest.ForGroup(ThingRequestGroup.BuildingArtificial), PathEndMode.InteractionCell, TraverseParms.For(pawn),
+                ThingRequest.ForUndefined(), PathEndMode.InteractionCell, TraverseParms.For(pawn),
                 maxDistFromPoint,
-                t => t.def.hasInteractionCell &&
-                     t.def.HasComp(typeof(CompMannable)) &&
-                     pawn.CanReserve(t) &&
-                     FindAmmoForTurret(pawn, t) != null);
+                null, turrets);
 
             if (thing == null)
             {
