0c2a6c5c4fa8e92dd80158b6b3777fb08ab8dd10
Karim Batbouta <kbatbouta66@gmail.com>
2020-08-28 14:06:28 -0700

    Fixed Items not being registered.

--- Source/CombatExtended/Harmony/Harmony_ApparelTracker.cs
+++ Source/CombatExtended/Harmony/Harmony_ApparelTracker.cs
@@ -10,25 +10,27 @@ namespace CombatExtended.HarmonyCE
     {
         internal static void Prefix(Pawn_ApparelTracker __instance, Apparel apparel)
         {
-            var hediffDef = apparel.def.GetModExtension<ApparelHediffExtension>()?.hediff;
-            if (hediffDef == null)
-                return;
-
             var pawn = __instance.pawn;
-
-            pawn.health.AddHediff(hediffDef);
-            if (apparel is ShieldBelt belt)
             {
-                pawn.hasShieldBelt = true;
-                if (pawn.hasShieldBelt)
-                    pawn.shieldBelt = belt;
+                if (apparel is Apparel_Shield shield)
+                {
+                    pawn.hasApparelShield = true;
+                    if (pawn.hasShieldBelt)
+                        pawn.apparelShield = shield;
+                }
             }
-
-            if (apparel is Apparel_Shield shield)
             {
-                pawn.hasApparelShield = true;
-                if (pawn.hasShieldBelt)
-                    pawn.apparelShield = shield;
+                var hediffDef = apparel.def.GetModExtension<ApparelHediffExtension>()?.hediff;
+                if (hediffDef != null)
+                {
+                    pawn.health.AddHediff(hediffDef);
+                    if (apparel is ShieldBelt belt)
+                    {
+                        pawn.hasShieldBelt = true;
+                        if (pawn.hasShieldBelt)
+                            pawn.shieldBelt = belt;
+                    }
+                }
             }
         }
     }
@@ -38,28 +40,33 @@ namespace CombatExtended.HarmonyCE
     {
         internal static void Prefix(Pawn_ApparelTracker __instance, Apparel apparel)
         {
-            var hediffDef = apparel.def.GetModExtension<ApparelHediffExtension>()?.hediff;
-            if (hediffDef == null)
-                return;
-
             var pawn = __instance.pawn;
-            var hediff = pawn.health.hediffSet.hediffs.FirstOrDefault(h => h.def == hediffDef);
-            if (hediff == null)
             {
-                Log.Warning($"Combat Extended :: Apparel {apparel} tried removing hediff {hediffDef} from {pawn} but could not find any");
-                return;
+                if (pawn.hasApparelShield && pawn.apparelShield is Apparel_Shield)
+                {
+                    pawn.hasShieldBelt = false;
+                    pawn.apparelShield = null;
+                }
             }
-            pawn.health.RemoveHediff(hediff);
-            if (pawn.hasShieldBelt && apparel is ShieldBelt)
             {
-                pawn.hasShieldBelt = false;
-                pawn.shieldBelt = null;
-            }
-
-            if (pawn.hasApparelShield && pawn.apparelShield is Apparel_Shield)
-            {
-                pawn.hasShieldBelt = false;
-                pawn.apparelShield = null;
+                var hediffDef = apparel.def.GetModExtension<ApparelHediffExtension>()?.hediff;
+                if (hediffDef != null)
+                {
+                    var hediff = pawn.health.hediffSet.hediffs.FirstOrDefault(h => h.def == hediffDef);
+                    if (hediff == null)
+                    {
+                        Log.Warning($"Combat Extended :: Apparel {apparel} tried removing hediff {hediffDef} from {pawn} but could not find any");
+                    }
+                    else
+                    {
+                        pawn.health.RemoveHediff(hediff);
+                        if (pawn.hasShieldBelt && apparel is ShieldBelt)
+                        {
+                            pawn.hasShieldBelt = false;
+                            pawn.shieldBelt = null;
+                        }
+                    }
+                }
             }
         }
     }
